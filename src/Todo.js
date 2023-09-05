699b062d0f60f9f041a0dbdd9d8b660076faf7e1
mohamedali678 <mohamedalidahir678@gmail.com>
2023-09-05 12:48:36 +0300

    Added image

--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,23 @@
 
 import {React, useState} from "react"
 import Header from "./components/Header"
+import { useRef } from "react";
 function Todo(){
 
     const [item, setItem] = useState("")
+    const [image, setImage] = useState(null);
     const [todo, setTodo] = useState([])
+
+    const imageRef = useRef()
 
     const addItem = (e) => {
         e.preventDefault()
         console.log(item)
-        if(item !== null){
-            setTodo([...todo, item])
+        if(item && image){
+            const newData = {item, image}
+            setTodo([...todo, newData])
+            setItem("")
+            imageRef.current.value = ""
         }
             // setTodo([...todo, item])
         
@@ -31,13 +38,17 @@ function Todo(){
 
             <form className="text-center">
                 <input value={item} onChange={(event) => {setItem(event.target.value)}} className="px-5 border border-blue-500 m-4 h-10  w-80 focus:border" type="text" placeholder="Enter item" />
-                <button className="bg-orange-500 p-2 text-white rounded" onClick={addItem}>Add item</button>
+                <input type="file" onChange={(e)=> setImage(e.target.files[0])} ref={imageRef} />
+                <button className="bg-orange-500 p-2 text-white rounded" onClick={addItem}>Post</button>
             </form>
 
             <div className="mx-64 text-center ">
                 {
                     todo.map((task, index) => (
-                        <p  className="bg-orange-400 text-white p-2 mx-60 rounded mt-2"> {task} </p>
+                        <>
+                        <p  className="text-black text-start p-2  rounded mt-2"> {task.item} </p>
+                        <img className="rounded" width="600px" src={URL.createObjectURL(task.image)} />
+                         </>
                     ))
                 }
             </div>
